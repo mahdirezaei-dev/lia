0668c5d71f6350cb0848e0c1f4eb51f91b4bea46
Matima <mahdirezaei.dev@gmail.com>
2024-02-10 13:27:53 +0330

    🧪 Create ProductControllerTest

---
+++ tests/Feature/ProductControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Testing\Fluent\AssertableJson;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Product;
+use App\Models\User;
+
+class ProductControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    
+}
