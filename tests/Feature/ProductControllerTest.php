c80440cb48b4df947df11bec69acb48a66a8ec71
Matima <mahdirezaei.dev@gmail.com>
2024-02-10 13:30:49 +0330

    ✅ Write Test for protected routes of Products

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -15,4 +15,17 @@ class ProductControllerTest extends TestCase
     use DatabaseMigrations;
 
     
+    /** @test */
+    public function unauthenticated_user_cant_accsees_to_protected_routes(): void
+    {
+        $product = Product::factory()->create();
+
+        $this->assertGuest($guard = null);
+
+        $this->getJson(route('products.index'))->assertStatus(Response::HTTP_UNAUTHORIZED);
+        $this->getJson(route('products.show', ['product' => $product->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);    
+        $this->postJson(route('products.store', []))->assertStatus(Response::HTTP_UNAUTHORIZED);    
+        $this->putJson(route('products.update', ['product' => $product->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);    
+        $this->deleteJson(route('products.destroy', ['product' => $product->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);    
+    }
 }
