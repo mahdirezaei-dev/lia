c92fa60e149c8f0c66fd94d778ff8664f5ee6b0b
Matima <mahdirezaei.dev@gmail.com>
2024-02-10 14:10:15 +0330

    ✅ Write test for delete product endpoint

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -105,6 +105,20 @@ class ProductControllerTest extends TestCase
     }
 
     /** @test */
+    public function user_can_delete_a_author(){
+        $product = Product::factory()->create();
+
+        $user = User::create($this->data);
+
+        $response = $this->withHeaders(['Authorization' => 'Bearer ' . auth()->tokenById($user->id)])->delete(route('products.destroy', ['product' => $product->id]));
+
+        $response->assertJsonStructure(['message'])
+                ->assertStatus(Response::HTTP_OK);
+
+        $this->assertDatabaseMissing('products', $product->toArray());
+    }
+
+    /** @test */
     public function unauthenticated_user_cant_accsees_to_protected_routes(): void
     {
         $product = Product::factory()->create();
