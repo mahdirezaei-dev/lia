5b71b241ea0157d8297eda5dafb89b8b38005577
Matima <mahdirezaei.dev@gmail.com>
2024-02-10 14:28:14 +0330

    🎨 Rename OrderControllerTest

---
+++ tests/Feature/OrderContollerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Testing\Fluent\AssertableJson;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Product;
+use App\Models\User;
+use App\Models\Product;
+
+class OrderControllerTest extends TestCase
+{
+     use DatabaseMigrations;
+
+    protected $data = [
+        'name' => 'mahdi',
+        'email' => 'mahdi@mail.com',
+        'password' => '12345678'
+    ];
+
+    /** @test */
+    public function unauthenticated_user_cant_accsees_to_protected_routes(): void
+    {
+        $order = Order::factory()->create();
+
+        $this->assertGuest($guard = null);
+
+        $this->getJson(route('orders.index'))->assertStatus(Response::HTTP_UNAUTHORIZED);
+        $this->getJson(route('orders.show', ['order' => $order->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);    
+        $this->postJson(route('orders.store', []))->assertStatus(Response::HTTP_UNAUTHORIZED);    
+        $this->putJson(route('orders.update', ['order' => $order->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);    
+        $this->deleteJson(route('orders.destroy', ['order' => $order->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);    
+    }
+}
