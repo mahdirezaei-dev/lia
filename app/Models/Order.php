f072f84edbb41e636df77f25c1269309119d2ae9
Matima <mahdirezaei.dev@gmail.com>
2024-02-10 11:17:58 +0330

    🐛 Fix model incompatibility with mongodb

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,9 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['total_price', 'user_id', 'products'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
