ead985217017f3db510701bcb9c3dd855902ec9e
Matima <mahdirezaei.dev@gmail.com>
2024-02-10 13:43:10 +0330

    🗃️ Write product migration

--- database/migrations/2024_02_10_073558_create_products_table.php
+++ database/migrations/2024_02_10_073558_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('inventory')->default(0);
+            $table->decimal('price')->default(0);
             $table->timestamps();
         });
     }
