417529be45061fee885b26fb6a641c7e573079ef
Matima <mahdirezaei.dev@gmail.com>
2024-02-10 13:44:02 +0330

    🗃️ Write order migration

--- database/migrations/2024_02_10_073700_create_orders_table.php
+++ database/migrations/2024_02_10_073700_create_orders_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->decimal('total_price');
+            $table->unsignedBigInteger('user_id');
+            $table->json('products')->nullable();
             $table->timestamps();
         });
     }
