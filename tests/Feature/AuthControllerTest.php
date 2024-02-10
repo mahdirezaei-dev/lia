c710681915502d8d5c7146b0bcc3ae45d3ec7e12
Matima <mahdirezaei.dev@gmail.com>
2024-02-10 10:28:22 +0330

    🦺 Create Login validation request

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class AuthControllerTest extends TestCase
 {
