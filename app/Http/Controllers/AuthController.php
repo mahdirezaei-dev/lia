a898f6d427333dd086e5d4a5a53f75151154a182
Matima <mahdirezaei.dev@gmail.com>
2024-02-11 12:46:59 +0330

    ♻️ Some minor refactor

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,114 +2,104 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Hash;
-use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
-class AuthController extends Controller
-{
-    /**
-     * Create a new AuthController instance.
-     */
-    public function __construct()
-    {
-        $this->middleware('auth:api', [
-            'except' => ['login', 'register']
-        ]);
-    }
+class AuthController extends Controller {
+	/**
+	 * Create a new AuthController instance.
+	 */
+	public function __construct() {
+		$this->middleware('auth:api', [
+			'except' => ['login', 'register'],
+		]);
+	}
 
-    /**
-     * Get a JWT via given credentials.
-     */
-    public function register(RegisterRequest $request): JsonResponse
-    {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+	/**
+	 * Get a JWT via given credentials.
+	 */
+	public function register(RegisterRequest $request): JsonResponse {
 
-        event(new Registered($user));
+		$user = User::create([
+			'name' => $request->name,
+			'email' => $request->email,
+			'password' => Hash::make($request->password),
+		]);
 
-        return $this->authenticate(
-            $request->validated()
-        );
-    }
+		event(new Registered($user));
 
-    /**
-     * Get a JWT via given credentials.
-     */
-    public function login(LoginRequest $request): JsonResponse
-    {
-        return $this->authenticate(
-            $request->validated()
-        );
-    }
+		return $this->authenticate(
+			$request->validated()
+		);
+	}
 
-    /**
-     * Get the authenticated User.
-     */
-    public function me(): JsonResponse
-    {
-        return response()->json(
-            auth()->user()
-        );
-    }
+	/**
+	 * Get a JWT via given credentials.
+	 */
+	public function login(LoginRequest $request): JsonResponse {
+		return $this->authenticate(
+			$request->validated()
+		);
+	}
 
-    /**
-     * Log the user out (Invalidate the token).
-     */
-    public function logout(): JsonResponse
-    {
-        auth()->logout();
+	/**
+	 * Get the authenticated User.
+	 */
+	public function me(): JsonResponse {
+		return response()->json(
+			auth()->user()
+		);
+	}
 
-        return response()->json([
-            'message' => 'auth.logged_out'
-        ]);
-    }
+	/**
+	 * Log the user out (Invalidate the token).
+	 */
+	public function logout(): JsonResponse {
+		auth()->logout();
 
-    /**
-     * Refresh a token.
-     */
-    public function refresh(): JsonResponse
-    {
-        return $this->respondWithToken(
-            auth()->refresh()
-        );
-    }
+		return response()->json([
+			'message' => 'auth.logged_out',
+		]);
+	}
 
+	/**
+	 * Refresh a token.
+	 */
+	public function refresh(): JsonResponse {
+		return $this->respondWithToken(
+			auth()->refresh()
+		);
+	}
 
-    /**
-     * Handle an authentication attempt.
-     */
-    protected function authenticate($request): JsonResponse
-    {
-        if (! $token = auth()->attempt($request)) {
-            return response()->json(
-                ['error' => 'auth.failed'],
-                Response::HTTP_UNAUTHORIZED
-            );
-        }
+	/**
+	 * Handle an authentication attempt.
+	 */
+	protected function authenticate($request): JsonResponse {
+		if (!$token = auth()->attempt($request)) {
+			return response()->json(
+				['error' => 'auth.failed'],
+				Response::HTTP_UNAUTHORIZED
+			);
+		}
 
-        return $this->respondWithToken($token);
-    }
+		return $this->respondWithToken($token);
+	}
 
-    /**
-     * Get the token array structure.
-     */
-    protected function respondWithToken(string $token): JsonResponse
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
-        ]);
-    }
+	/**
+	 * Get the token array structure.
+	 */
+	protected function respondWithToken(string $token): JsonResponse {
+		return response()->json([
+			'access_token' => $token,
+			'token_type' => 'bearer',
+			'expires_in' => auth()->factory()->getTTL() * 60,
+		]);
+	}
 }
--- tests/Feature/OrderControllerTest.php
+++ tests/Feature/OrderControllerTest.php
@@ -2,175 +2,168 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Testing\Fluent\AssertableJson;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use App\Models\Order;
 use App\Models\Product;
 use App\Models\User;
-use App\Models\Order;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
 
-class OrderControllerTest extends TestCase
-{
-     use DatabaseMigrations;
+class OrderControllerTest extends TestCase {
+	use DatabaseMigrations;
 
-    protected $data = [
-        'name' => 'mahdi',
-        'email' => 'mahdi@mail.com',
-        'password' => '12345678'
-    ];
+	protected $data = [
+		'name' => 'mahdi',
+		'email' => 'mahdi@mail.com',
+		'password' => '12345678',
+	];
 
-    /** @test */
-    public function authenticated_user_can_get_all_orders(): void
-    {
-        $user = User::create($this->data);
+	/** @test */
+	public function authenticated_user_can_get_all_orders(): void {
+		$user = User::create($this->data);
 
-        Order::factory()->count(20)->create();
+		Order::factory()->count(20)->create();
 
-        $response = $this->withHeaders([
-                'Authorization' => 'Bearer ' . auth()->tokenById($user->id),
-            ])
-            ->getJson(route('orders.index'));
-        
-        $this->assertAuthenticatedAs($user, $guard = null);
-        
-        $response->assertJsonCount(20, 'data')
-            ->assertStatus(Response::HTTP_OK);
-    }
+		$response = $this->withHeaders([
+			'Authorization' => 'Bearer ' . auth()->tokenById($user->id),
+		])
+			->getJson(route('orders.index'));
 
-    /** @test */
-    public function authenticated_user_can_get_an_order(): void
-    {
-        $user = User::create($this->data);
+		$this->assertAuthenticatedAs($user, $guard = null);
 
-        $order = Order::factory()->create();
+		$response->assertJsonCount(20, 'data')
+			->assertStatus(Response::HTTP_OK);
+	}
 
-        $response = $this->withHeaders([
-                'Authorization' => 'Bearer ' . auth()->tokenById($user->id),
-            ])
-            ->getJson(route('orders.show', ['order' => $order->id]));
-        
-        $this->assertAuthenticatedAs($user, $guard = null);
+	/** @test */
+	public function authenticated_user_can_get_an_order(): void {
+		$user = User::create($this->data);
 
-        $response->assertJsonStructure([
-            'success',
-            'code',
-            'message',
-            ])
-            ->assertStatus(Response::HTTP_OK);
-    }
+		$order = Order::factory()->create();
 
-    /** @test */
-    public function user_can_create_a_new_order(){
+		$response = $this->withHeaders([
+			'Authorization' => 'Bearer ' . auth()->tokenById($user->id),
+		])
+			->getJson(route('orders.show', ['order' => $order->id]));
 
-        $products = Product::factory()->count(3)->create();
+		$this->assertAuthenticatedAs($user, $guard = null);
 
-        $order = [
-            'products' => [],
-            'total_price' => 100.22
-        ];
-        foreach ($products as $key => $value) {
-           $order['products'][] = [
-                'id' => $value->id,
-                'quantity' => 1,
-           ];
-        }
+		$response->assertJsonStructure([
+			'success',
+			'code',
+			'message',
+		])
+			->assertStatus(Response::HTTP_OK);
+	}
 
-        $user = User::create($this->data);
+	/** @test */
+	public function user_can_create_a_new_order() {
 
-        $response = $this->withHeaders(['Authorization' => 'Bearer ' . auth()->tokenById($user->id)])
-                ->postJson(
-                    route('orders.store'),
-                    $order
-                )
-                ->assertJsonStructure(['data' => ['products', 'total_price']])
-                ->assertStatus(Response::HTTP_CREATED);
+		$products = Product::factory()->count(3)->create();
 
-        $this->assertDatabaseHas('orders', $order);
-    }
+		$order = [
+			'products' => [],
+			'total_price' => 100.22,
+		];
+		foreach ($products as $key => $value) {
+			$order['products'][] = [
+				'id' => $value->id,
+				'quantity' => 1,
+			];
+		}
 
-        /** @test */
-    public function user_can_update_an_existing_order(){
+		$user = User::create($this->data);
 
-        $products = Product::factory()->count(3)->create();
-        $existing_order = Order::factory()->create();
+		$response = $this->withHeaders(['Authorization' => 'Bearer ' . auth()->tokenById($user->id)])
+			->postJson(
+				route('orders.store'),
+				$order
+			)
+			->assertJsonStructure(['data' => ['products', 'total_price']])
+			->assertStatus(Response::HTTP_CREATED);
 
-        $order = [
-            'products' => [],
-            'total_price' => 100.22
-        ];
-        foreach ($products as $key => $value) {
-           $order['products'][] = [
-                'id' => $value->id,
-                'quantity' => 1,
-           ];
-        }
+		$this->assertDatabaseHas('orders', $order);
+	}
 
-        $user = User::create($this->data);
+	/** @test */
+	public function user_can_update_an_existing_order() {
 
-        $this->withHeaders(['Authorization' => 'Bearer ' . auth()->tokenById($user->id)])
-                ->patchJson(route('orders.update', ['order' => $existing_order->id]), $order)
-                // ->assertJsonStructure(['data' => ['products', 'total_price']])
-                ->assertStatus(Response::HTTP_OK);
+		$products = Product::factory()->count(3)->create();
+		$existing_order = Order::factory()->create();
 
-        $this->assertDatabaseMissing('orders', $existing_order->toArray());
-        $this->assertDatabaseHas('orders', $order);
-    }
+		$order = [
+			'products' => [],
+			'total_price' => 100.22,
+		];
+		foreach ($products as $key => $value) {
+			$order['products'][] = [
+				'id' => $value->id,
+				'quantity' => 1,
+			];
+		}
 
-        /** @test */
-    public function user_can_delete_an_order(){
-        $order = Order::factory()->create();
+		$user = User::create($this->data);
 
-        $user = User::create($this->data);
+		$this->withHeaders(['Authorization' => 'Bearer ' . auth()->tokenById($user->id)])
+			->patchJson(route('orders.update', ['order' => $existing_order->id]), $order)
+		// ->assertJsonStructure(['data' => ['products', 'total_price']])
+			->assertStatus(Response::HTTP_OK);
 
-        $response = $this->withHeaders(['Authorization' => 'Bearer ' . auth()->tokenById($user->id)])->delete(route('orders.destroy', ['order' => $order->id]));
+		$this->assertDatabaseMissing('orders', $existing_order->toArray());
+		$this->assertDatabaseHas('orders', $order);
+	}
 
-        $response->assertJsonStructure(['message'])
-                ->assertStatus(Response::HTTP_OK);
+	/** @test */
+	public function user_can_delete_an_order() {
+		$order = Order::factory()->create();
 
-        $this->assertDatabaseMissing('orders', $order->toArray());
-    }
+		$user = User::create($this->data);
 
-    /** @test */
-    public function unauthenticated_user_cant_accsees_to_protected_routes(): void
-    {
-        $order = Order::factory()->create();
+		$response = $this->withHeaders(['Authorization' => 'Bearer ' . auth()->tokenById($user->id)])->delete(route('orders.destroy', ['order' => $order->id]));
 
-        $this->assertGuest($guard = null);
+		$response->assertJsonStructure(['message'])
+			->assertStatus(Response::HTTP_OK);
 
-        $this->getJson(route('orders.index'))->assertStatus(Response::HTTP_UNAUTHORIZED);
-        $this->getJson(route('orders.show', ['order' => $order->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);    
-        $this->postJson(route('orders.store', []))->assertStatus(Response::HTTP_UNAUTHORIZED);    
-        $this->putJson(route('orders.update', ['order' => $order->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);    
-        $this->deleteJson(route('orders.destroy', ['order' => $order->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);    
-    }
-    /** @test */
-    public function quantity_of_products_in_an_order_cannot_exceed_the_stock(){
-        $products = Product::create([
-            'name' => 'Samsoung A20 Mobile',
-            'inventory' => 5,
-            'price' => 3,
-        ]);
+		$this->assertDatabaseMissing('orders', $order->toArray());
+	}
 
-        $order = [
-            'products' => [
-                'id' => $products->id,
-                'quantity' => $products->inventory + 1
-            ],
-            'total_price' => 100.22
-        ];
+	/** @test */
+	public function unauthenticated_user_cant_accsees_to_protected_routes(): void {
+		$order = Order::factory()->create();
 
+		$this->assertGuest($guard = null);
 
-        $user = User::create($this->data);
+		$this->getJson(route('orders.index'))->assertStatus(Response::HTTP_UNAUTHORIZED);
+		$this->getJson(route('orders.show', ['order' => $order->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);
+		$this->postJson(route('orders.store', []))->assertStatus(Response::HTTP_UNAUTHORIZED);
+		$this->putJson(route('orders.update', ['order' => $order->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);
+		$this->deleteJson(route('orders.destroy', ['order' => $order->id]))->assertStatus(Response::HTTP_UNAUTHORIZED);
+	}
+	/** @test */
+	public function quantity_of_products_in_an_order_cannot_exceed_the_stock() {
+		$products = Product::create([
+			'name' => 'Samsoung A20 Mobile',
+			'inventory' => 5,
+			'price' => 3,
+		]);
 
-        $response = $this->withHeaders(['Authorization' => 'Bearer ' . auth()->tokenById($user->id)])
-                ->postJson(
-                    route('orders.store'),
-                    $order
-                )
-                ->assertStatus(422);
+		$order = [
+			'products' => [
+				'id' => $products->id,
+				'quantity' => $products->inventory + 1,
+			],
+			'total_price' => 100.22,
+		];
 
-        $this->assertDatabaseMissing('orders', $order);
-    }
+		$user = User::create($this->data);
+
+		$response = $this->withHeaders(['Authorization' => 'Bearer ' . auth()->tokenById($user->id)])
+			->postJson(
+				route('orders.store'),
+				$order
+			)
+			->assertStatus(422);
+
+		$this->assertDatabaseMissing('orders', $order);
+	}
 }
